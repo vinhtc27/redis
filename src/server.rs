68cea2f9c3d57fe1acd3c7d957284132fb01403f
vinhtc27 <vinhtc27@gmail.com>
2024-04-17 09:10:01 +0700

    pass 12th stage

--- src/server.rs
+++ src/server.rs
@@ -144,12 +144,12 @@ pub async fn run(
 
     let client = match master {
         Some(address) => {
-            let port = config.server_tcp_port().to_string();
+            // let port = config.server_tcp_port().to_string();
 
             let mut client = ReplicaClient::connect(address).await?;
             let _ = client.ping(None).await?;
-            let _ = client.replconf("listening-port", port.into()).await?;
-            let _ = client.psync("?", -1).await?;
+            // let _ = client.replconf("listening-port", port.into()).await?;
+            // let _ = client.psync("?", -1).await?;
 
             Some(client)
         }
